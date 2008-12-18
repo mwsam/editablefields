f55b489f9794302fb50e309e8704cf8d146171b2
Mark Burton <markfoodyburton@114447.no-reply.drupal.org>
2008-12-18 21:54:27 +0000

    adding editablefields.js

---
+++ editablefields.js
@@ -0,0 +1,75 @@
+
+editablefields = function() {
+  $('div.editablefields').not('.noonload').each(function() {
+                                                $(this).children().css( 'opacity', '0.3' ); 
+                                                editablefieldsload(this,"/editablefields_html/"+$(this).attr("nid")+"/"+$(this).attr("field"));
+                                              });
+
+
+  $('div.editablefields').change(editablefieldschanger);
+  $('div.editablefields').submit(editablefieldschanger);
+};
+  
+var updateValue;
+
+editablefieldsupdater = function(element) 
+{
+  var nid=$(element).attr("nid");
+  $(element).children().css( 'opacity', '0.3' ); 
+  
+  $.ajax({
+  type: "POST",
+        url: "/editablefields_submit",
+        data: $(element).find('form').serialize()+"&nid="+nid,
+        element: $(element),
+        success: function(msg){
+            updateValue=0;
+            editablefieldsload($(this.element),"/editablefields_html/"+$(this.element).attr("nid")+"/"+$(this.element).attr("field"));
+      },
+        error: function(msg){
+        alert( "Error, unable to make update: " + msg.responseText );
+      }
+    });
+};
+
+editablefieldsload = function( e, url) 
+{
+  $(e).ajaxSubmit({
+            url: url,
+            type: 'GET',
+            success: function(response) {
+              // Call all callbacks.
+              if (response.__callbacks) {
+                $.each(response.__callbacks, function(i, callback) {
+                  eval(callback)(e, response);
+//                         alert(response.scripts.inline);                         
+                });
+              }
+              $(e).html(response.content);
+              $(e).find(':input').change(editablefieldschanger);
+              $(e).find(':input').submit(editablefieldschanger);
+            },
+            error: function(response) { alert(Drupal.t("An error occurred at ") + url ); },
+            dataType: 'json'
+          });
+}
+  
+editablefieldschanger = function() 
+{
+  var t=this;
+  
+  if (! $(this).hasClass('editablefields') ) {
+    t=$(this).parents('div.editablefields');
+  }
+
+  newValue=$(t).find('form').serialize();
+  if (newValue!=updateValue) {
+    updateValue=newValue;
+    editablefieldsupdater($(t));
+  }
+  return false;// dont actually submit
+};
+
+if (Drupal.jsEnabled) {
+  $(document).ready(editablefields);
+}
